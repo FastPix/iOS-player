21f6b3c482034ef4d33c4defe4a248a0dcdcab8f
D. Neha Reddy <neha.r@fastpix.io>
2026-06-08 14:34:38 +0530

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "FastPixPlayerSDKTest",
+    name: "FastPixPlayerSDK",
     
     platforms: [
         .iOS(.v13)
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "FastPixPlayerSDKTest",
-            targets: ["FastPixPlayerSDKTest"]),
+            name: "FastPixPlayerSDK",
+            targets: ["FastPixPlayerSDK"]),
     ],
     
     dependencies: [
@@ -26,13 +26,13 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FastPixPlayerSDKTest",
+            name: "FastPixPlayerSDK",
             dependencies: [
                 .product(name: "FastpixVideoDataAVPlayer", package: "iOS-data-avplayer-sdk")  // Link the Git package to your local package
             ]
         ),
         .testTarget(
-            name: "FastPixPlayerSDKTestTests",
-            dependencies: ["FastPixPlayerSDKTest"]),
+            name: "FastPixPlayerSDKTests",
+            dependencies: ["FastPixPlayerSDK"]),
     ]
 )
